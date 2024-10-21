360cd64c40d8c8c3708a903f6e132feb77611033
minijeans <129875801+minijeans@users.noreply.github.com>
2024-10-21 23:29:35 +0900

    [level 0] Title: 2의 영역, Time: 2.46 ms, Memory: 13.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,7 @@
+def solution(arr):
+    if arr.count(2) >= 2:
+        return arr[arr.index(2):len(arr)-arr[::-1].index(2)]
+    elif arr.count(2) == 1:
+        return [2]
+    else:
+        return [-1]
