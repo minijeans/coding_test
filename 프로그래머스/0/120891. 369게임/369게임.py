1a37a62f83f3af9e96830e7f88ecc692b3bf23db
minijeans <129875801+minijeans@users.noreply.github.com>
2024-10-24 03:33:06 +0900

    [level 0] Title: 369게임, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -3,7 +3,7 @@ def solution(order):
     num = 0
     while order != 0:
         num = order % 10 
-        if num == 3 or num == 6 or num == 9:
+        if num in (3, 6, 9):
             result += 1
         order //= 10
     return result
